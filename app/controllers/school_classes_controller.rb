a542bf964a6d36c397a43e76ee33309648594b4c
quinncidences <qrc.inpa@gmail.com>
2019-04-12 16:03:39 -0700

    Done.

---
+++ app/controllers/school_classes_controller.rb
@@ -0,0 +1,32 @@
+class SchoolClassesController < ApplicationController
+
+
+    def show
+      @school_class = SchoolClass.find(params[:id])
+    end
+
+
+    def new
+      @school_class = SchoolClass.new
+    end
+
+
+    def create
+      @school_class = SchoolClass.create(params.require(:school_class).permit(:title, :room_number))
+      redirect_to @school_class
+    end
+
+
+    def edit
+      @school_class = SchoolClass.find(params[:id])
+    end
+
+
+    def update
+      @schoolclass = SchoolClass.find(params[:id])
+      @school_class = SchoolClass.update(params.require(:school_class).permit(:title, :room_number))
+      redirect_to @school_class
+    end
+
+
+end
